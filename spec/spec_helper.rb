688b5e17554df2ba127f50bbc0f3847a45428e65
Kyle Smith <kyle@localhost.localdomain>
2014-05-24 14:37:55 -0400

    Getting started with rspec.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+puts 'Spec helper loaded.'
+
+# Set a known account, character, etc. for integration tests.
+INTEGRATION_REGION = 'us'
+INTEGRATION_BATTLE_TAG = 'Segfault#1483'
+INTEGRATION_CHARACTER_NAME = 'Dhreyic'
+
+# Fill out expected information about the character, to be asserted in
+# integration tests.
+INTEGRATION_CHARACTER_IS_HC = false
+INTEGRATION_CHARACTER_LEVEL = 70
